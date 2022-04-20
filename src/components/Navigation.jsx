1a8c4df8a5d568f421836e653646b4292d6f6f5b
GustafGronlund <gustafgronlund@gmail.com>
2022-04-20 14:48:03 +0200

    Installed React and some basic styling.

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+import "../styles/Navigation.scss";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <h2>🛸 SWAPI Wiki</h2>
+      <ul>
+        <li>
+          <a href="">item 1</a>
+        </li>
+        <li>
+          <a href="">item 2</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
